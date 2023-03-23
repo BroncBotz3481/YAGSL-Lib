431da9bf727557cf133bf0bc760aa9568ad019c1
thenetworkgrinch <thenetworkgrinch@users.noreply.github.com>
2023-03-22 20:03:34 -0500

    Updated YAGSL

--- src/main/java/swervelib/SwerveController.java
+++ src/main/java/swervelib/SwerveController.java
@@ -15,7 +15,7 @@ public class SwerveController {
    */
   public final SwerveControllerConfiguration config;
   /** PID Controller for the robot heading. */
-  private final PIDController thetaController; // TODO: Switch to ProfilePIDController
+  public final PIDController thetaController; // TODO: Switch to ProfilePIDController
   /** Last angle as a scalar [-1,1] the robot was set to. */
   public double lastAngleScalar;
   /** {@link SlewRateLimiter} for movement in the X direction in meters/second. */
