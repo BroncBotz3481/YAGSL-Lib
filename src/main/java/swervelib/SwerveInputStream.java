8dc767eb078d6f7d467f0c38101d3c2affa62ec5
thenetworkgrinch <thenetworkgrinch@users.noreply.github.com>
2024-12-14 00:10:27 +0000

    Upgrading to 2025.1.0.0.10

--- src/main/java/swervelib/SwerveInputStream.java
+++ src/main/java/swervelib/SwerveInputStream.java
@@ -159,7 +159,7 @@ public class SwerveInputStream implements Supplier<ChassisSpeeds> {
    * @param rot Rotation axis with values from [-1, 1]
    * @return self
    */
-  public SwerveInputStream withRotation(DoubleSupplier rot) {
+  public SwerveInputStream withRotationControllerAxis(DoubleSupplier rot) {
     controllerOmega = Optional.of(rot);
     return this;
   }
@@ -171,7 +171,8 @@ public class SwerveInputStream implements Supplier<ChassisSpeeds> {
    * @param headingY Heading Y axis with values from [-1, 1]
    * @return self
    */
-  public SwerveInputStream withHeading(DoubleSupplier headingX, DoubleSupplier headingY) {
+  public SwerveInputStream withHeadingControllerAxis(
+      DoubleSupplier headingX, DoubleSupplier headingY) {
     controllerHeadingX = Optional.of(headingX);
     controllerHeadingY = Optional.of(headingY);
     return this;
@@ -322,12 +323,12 @@ public class SwerveInputStream implements Supplier<ChassisSpeeds> {
         return SwerveInputMode.HEADING;
       } else {
         DriverStation.reportError(
-            "Attempting to enter HEADING mode without heading axis, please use SwerveInputStream.withHeading to add heading axis!",
+            "Attempting to enter HEADING mode without heading axis, please use SwerveInputStream.withHeadingControllerAxis to add heading axis!",
             false);
       }
     } else if (controllerOmega.isEmpty()) {
       DriverStation.reportError(
-          "Attempting to enter ANGULAR_VELOCITY mode without a rotation axis, please use SwerveInputStream.withRotation to add angular velocity axis!",
+          "Attempting to enter ANGULAR_VELOCITY mode without a rotation axis, please use SwerveInputStream.withRotationControllerAxis to add angular velocity axis!",
           false);
       return SwerveInputMode.TRANSLATION_ONLY;
     }
